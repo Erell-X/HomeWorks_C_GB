a06c49684e287c8bcdbc9bbe32c31a58fe7713ee
*Elizarov <Super.elizarov1451@mail.ru>
2023-04-30 18:27:52 +0300

    Добавлены программы к задачам №№ 1-3 ДЗ-2

---
+++ HW_2/Task_2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Num, Res, lastFigure;
+  
+  lastFigure = 0;
+  Res = 0;
+  
+  Console.Clear();
+  Console.WriteLine("Введите число: ");
+  Num = Convert.ToInt32(Console.ReadLine());
+  
+  Res = Num;
+  
+  if (Num > 99 && Num < 1000)
+  {
+    lastFigure = Num % 10;
+    
+    Console.WriteLine($"Третья цифра заданного числа равна {lastFigure}");
+  }
+  
+  else if (Num < 100)
+  {
+    Console.WriteLine("Третьей цифры у заданного числа нет");
+  }
+  
+  
+  while (Res > 999)
+  {
+    Res = Res / 10;
+  
+    if (Res > 99 && Res < 1000)
+    {
+    lastFigure = Res % 10;
+    Console.WriteLine($"Третья цифра заданного числа равна {lastFigure}");
+    }
+  }
---
+++ HW_2/Task_3/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+
+int Day;
+  
+  Console.WriteLine("Введите день недели:");
+  Day = Convert.ToInt32(Console.ReadLine());
+  
+  switch(Day) {
+    
+    case 1:
+      {
+          Console.WriteLine("Не является выходным днём");
+          Console.WriteLine();
+          break;
+      }
+
+    case 2:
+      {
+          Console.WriteLine("Не является выходным днём");
+          Console.WriteLine();
+          break;
+      }
+
+    case 3:
+      {
+          Console.WriteLine("Не является выходным днём");
+          Console.WriteLine();
+          break;
+      }
+
+    case 4:
+      {
+          Console.WriteLine("Не является выходным днём");
+          Console.WriteLine();
+          break;
+      }
+
+    case 5:
+      {
+          Console.WriteLine("Не является выходным днём");
+          Console.WriteLine();
+          break;
+      }
+
+    case 6:
+      {
+          Console.WriteLine("Выходной день");
+          Console.WriteLine();
+          break;
+      }
+
+    case 7:
+      {
+          Console.WriteLine("Выходной день");
+          Console.WriteLine();
+          break;
+      }
+    
+    default:
+      {
+          Console.WriteLine("Нет такого дня недели");
+          Console.WriteLine();
+          break;
+      }
+
+  }

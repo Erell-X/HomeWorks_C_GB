fb1c14bd8b1d67a30b91b7db87b29e8166739eca
*Elizarov <Super.elizarov1451@mail.ru>
2023-04-30 18:23:54 +0300

    Добавлены программы к задачам №№ 1-4 ДЗ-1

---
+++ HW_1/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+
+int Number1, Number2;
+
+Console.WriteLine("Введите 1-е число");
+Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2-е число");
+Number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 > Number2)
+{
+  Console.WriteLine($"Max = {Number1} (1-е число)");
+}
+
+else if (Number1 < Number2)
+{
+  Console.WriteLine($"Max = {Number2} (2-е число)");
+}
+
+else if (Number1 == Number2)
+{
+  Console.WriteLine($"Числа равны друг другу");
+}
---
+++ HW_1/Task_4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int N, F;
+  
+  F = 1;
+  
+  Console.Clear();
+  Console.Write("Введите число: ");
+  
+  N = Convert.ToInt32(Console.ReadLine());
+
+  Console.WriteLine();
+  Console.Write("Ряд чётных чисел: ");
+
+  while (F <= N-2)
+  {
+    if (F % 2 == 0)
+    {
+    Console.Write($"{F}, ");
+    }
+  
+    F++;
+  }
+    
+    if (F % 2 == 0)
+    {
+      Console.Write(F);
+    }
+    
+    else if (F % 2 == 1 && N > 1)
+    {
+      F++;
+      Console.Write(F);
+    }
